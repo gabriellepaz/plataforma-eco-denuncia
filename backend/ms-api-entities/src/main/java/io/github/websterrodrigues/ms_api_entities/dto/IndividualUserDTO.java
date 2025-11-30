6ca839c95b8110b8b1c394226172c7befc4fa1e8
Webster Diogenes <web.driw12@gmail.com>
2025-11-30 13:54:52 -0300

    fix: Correct mapStruct and create RoleRepository

--- backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/dto/IndividualUserDTO.java
+++ backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/dto/IndividualUserDTO.java
@@ -1,11 +1,13 @@
 package io.github.websterrodrigues.ms_api_entities.dto;
 
+import io.github.websterrodrigues.ms_api_entities.model.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
+import java.util.Set;
 
 public record IndividualUserDTO(
 
@@ -23,6 +25,6 @@ public record IndividualUserDTO(
     String sex,
 
     @NotEmpty(message = "Declare ao menos uma role!")
-    List<@NotBlank(message = "Verifique se não há campos vazios!") String> roles) {
+    Set<@NotBlank(message = "Verifique se não há campos vazios!") String> roles) {
 
 }
--- backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/dto/mappers/IndividualUserMapper.java
+++ backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/dto/mappers/IndividualUserMapper.java
@@ -2,11 +2,25 @@ package io.github.websterrodrigues.ms_api_entities.dto.mappers;
 
 import io.github.websterrodrigues.ms_api_entities.dto.IndividualUserDTO;
 import io.github.websterrodrigues.ms_api_entities.model.IndividualUser;
+import io.github.websterrodrigues.ms_api_entities.model.Role;
+import io.github.websterrodrigues.ms_api_entities.repository.RoleRepository;
 import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface IndividualUserMapper {
+public abstract class IndividualUserMapper {
 
-    IndividualUser toEntity(IndividualUserDTO dto);
-    IndividualUserDTO toDto(IndividualUser user);
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public abstract IndividualUser toEntity(IndividualUserDTO dto);
+    public abstract   IndividualUserDTO toDto(IndividualUser user);
+
+    protected Role mapRole(String roleName) {
+        return roleRepository.findByName(roleName);
+    }
+
+    protected String mapRole(Role role) {
+        return role.getName();
+    }
 }
