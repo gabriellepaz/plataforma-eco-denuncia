a68551152eb563b17d10af6464c5d78dde224b2a
Webster Diogenes <web.driw12@gmail.com>
2025-11-30 18:08:22 -0300

    feat: create entities

---
+++ backend/ms-api-complaint/src/main/java/io/github/websterrodrigues/ms_api_complaint/MsApiComplaintApplication.java
@@ -0,0 +1,13 @@
+package io.github.websterrodrigues.ms_api_complaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiComplaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiComplaintApplication.class, args);
+	}
+
+}
--- backend/ms-api-complaint/src/main/java/io/github/websterrodrigues/ms_api_complaint/model/Attachment.java
+++ backend/ms-api-complaint/src/main/java/io/github/websterrodrigues/ms_api_complaint/model/Attachment.java
@@ -1,4 +1,107 @@
 package io.github.websterrodrigues.ms_api_complaint.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "tb_attachment")
 public class Attachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "arquivo")
+    private String file;
+
+    @Column(name = "nome_arquivo")
+    private String extension;
+
+    @Column(name = "tamanho_mb", nullable = false)
+    private Double sizeInMb;
+
+    @CreatedDate
+    @Column(name = "data_anexo", updatable = false)
+    private LocalDateTime attachmentDate;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao_anexo")
+    private LocalDateTime dateAttachmentUpdatedate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "complaint_id")
+    private Complaint complaint;
+
+    public Attachment() {
+
+    }
+
+    public Attachment(UUID id, String file, String extension, Double sizeInMb, LocalDateTime attachmentDate, LocalDateTime dateAttachmentUpdatedate, Complaint complaint) {
+        this.id = id;
+        this.file = file;
+        this.extension = extension;
+        this.sizeInMb = sizeInMb;
+        this.attachmentDate = attachmentDate;
+        this.dateAttachmentUpdatedate = dateAttachmentUpdatedate;
+        this.complaint = complaint;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public Double getSizeInMb() {
+        return sizeInMb;
+    }
+
+    public void setSizeInMb(Double sizeInMb) {
+        this.sizeInMb = sizeInMb;
+    }
+
+    public LocalDateTime getAttachmentDate() {
+        return attachmentDate;
+    }
+
+    public void setAttachmentDate(LocalDateTime attachmentDate) {
+        this.attachmentDate = attachmentDate;
+    }
+
+    public LocalDateTime getDateAttachmentUpdatedate() {
+        return dateAttachmentUpdatedate;
+    }
+
+    public void setDateAttachmentUpdatedate(LocalDateTime dateAttachmentUpdatedate) {
+        this.dateAttachmentUpdatedate = dateAttachmentUpdatedate;
+    }
+
+    public Complaint getComplaint() {
+        return complaint;
+    }
+
+    public void setComplaint(Complaint complaint) {
+        this.complaint = complaint;
+    }
 }
