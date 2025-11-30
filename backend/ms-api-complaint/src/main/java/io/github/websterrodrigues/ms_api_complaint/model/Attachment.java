e88be973588aa7d36011d84f08c19a43d03d0899
Webster Diogenes <web.driw12@gmail.com>
2025-11-30 18:07:35 -0300

    feat: create entities

---
+++ backend/ms-api-complaint/src/main/java/io/github/websterrodrigues/ms_api_complaint/model/Attachment.java
@@ -0,0 +1,4 @@
+package io.github.websterrodrigues.ms_api_complaint.model;
+
+public class Attachment {
+}
---
+++ backend/ms-api-complaint/src/main/java/io/github/websterrodrigues/ms_api_complaint/model/enums/OperationStatus.java
@@ -0,0 +1,4 @@
+package io.github.websterrodrigues.ms_api_complaint.model.enums;
+
+public enum OperationStatus {
+}
