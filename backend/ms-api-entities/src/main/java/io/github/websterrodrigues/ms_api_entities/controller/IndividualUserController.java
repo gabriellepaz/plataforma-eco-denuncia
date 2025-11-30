be8be6951f131b6879db70eef4f726e1940b3aeb
Webster Diogenes <web.driw12@gmail.com>
2025-11-30 02:44:46 -0300

    feat: add IndividualUserController

--- backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/controller/IndividualUserController.java
+++ backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/controller/IndividualUserController.java
@@ -1,28 +1,58 @@
 package io.github.websterrodrigues.ms_api_entities.controller;
 
 import io.github.websterrodrigues.ms_api_entities.dto.IndividualUserDTO;
+import io.github.websterrodrigues.ms_api_entities.dto.mappers.IndividualUserMapper;
 import io.github.websterrodrigues.ms_api_entities.model.IndividualUser;
 import io.github.websterrodrigues.ms_api_entities.service.IndividualUserService;
+import io.github.websterrodrigues.ms_api_entities.utils.GenericController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
-public class IndividualUserController {
+public class IndividualUserController implements GenericController {
 
     private final IndividualUserService service;
+    private final IndividualUserMapper mapper;
 
-    public IndividualUserController(IndividualUserService service) {
+    public IndividualUserController(IndividualUserMapper mapper, IndividualUserService service) {
+        this.mapper = mapper;
         this.service = service;
     }
 
     @PostMapping
     public ResponseEntity<Void> save(@RequestBody IndividualUserDTO userDTO){
 
+        IndividualUser user = mapper.toEntity(userDTO);
+        service.save(user);
+        URI location = generateHeaderLocation(user.getId());
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody IndividualUserDTO userDTO){
+
+        UUID idUser = UUID.fromString(id);
+        IndividualUser user = mapper.toEntity(userDTO);
+        user.setId(idUser); //Garantir que o ID seja definido corretamente NÃO REMOVER
+        service.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<Void> delete(@PathVariable String id){
+
+        service.delete(UUID.fromString(id));
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<IndividualUserDTO> findById(@PathVariable String id){
+        IndividualUserDTO dto = mapper.toDto(service.findById(UUID.fromString(id)));
+        return ResponseEntity.ok(dto);
     }
 
 
---
+++ backend/ms-api-entities/src/main/java/io/github/websterrodrigues/ms_api_entities/utils/GenericController.java
@@ -0,0 +1,17 @@
+package io.github.websterrodrigues.ms_api_entities.utils;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+public interface GenericController {
+
+    default URI generateHeaderLocation(UUID id){
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
