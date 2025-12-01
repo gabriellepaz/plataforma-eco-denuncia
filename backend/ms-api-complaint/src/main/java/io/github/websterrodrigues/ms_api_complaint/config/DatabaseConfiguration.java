f010fc6a6d543ffd36bc99354965a2f7a82a1383
Webster Diogenes <web.driw12@gmail.com>
2025-12-01 00:09:47 -0300

    feat: create a structure for connecting to a database

---
+++ backend/ms-api-complaint/src/main/java/io/github/websterrodrigues/ms_api_complaint/config/DatabaseConfiguration.java
@@ -0,0 +1,44 @@
+package io.github.websterrodrigues.ms_api_complaint.config;
+
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DatabaseConfiguration {
+//
+//    @Value("${spring.datasource.url}")
+//    String url;
+//
+//    @Value("${spring.datasource.username}")
+//    String username;
+//
+//    @Value("${spring.datasource.password}")
+//    String password;
+//
+//    @Value("${spring.datasource.driver-class-name}")
+//    String driver;
+//
+//    @Bean
+//    public DataSource hikariDataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(url);
+//        config.setUsername(username);
+//        config.setPassword(password);
+//        config.setDriverClassName(driver);
+//
+//        config.setMaximumPoolSize(10); //Máximo de conexões
+//        config.setMinimumIdle(1); //Tamanho inicial do pool
+//        config.setPoolName("complaint-db-pool");
+//        config.setMaxLifetime(600000); //Tempo máximo de vida da conexão 600 mil ms (10 min) -> Gerencia conexões
+//        config.setConnectionTimeout(100000); //Timeout de conexão
+//        config.setConnectionTestQuery("select 1"); //Teste de conexão
+//
+//        return new HikariDataSource(config); //Hikari DataSource exige uma HikariConfig
+//    }
+//}
+//Desativado temporariamente para o h2 enquanto não há modelagem de banco complexa
