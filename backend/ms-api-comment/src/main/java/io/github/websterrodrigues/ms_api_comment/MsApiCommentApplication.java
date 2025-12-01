dc2dc41c93c07e3feb8f12204089f03c7cc4a7a9
Webster Diogenes <web.driw12@gmail.com>
2025-12-01 01:31:47 -0300

    chore: initial commit

---
+++ backend/ms-api-comment/src/main/java/io/github/websterrodrigues/ms_api_comment/MsApiCommentApplication.java
@@ -0,0 +1,13 @@
+package io.github.websterrodrigues.ms_api_comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiCommentApplication.class, args);
+	}
+
+}
